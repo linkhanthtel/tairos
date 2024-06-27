f2da575b89610dedbb211d469f5bb2620acce094
Lin Khant Htel <linkhanthtel@icloud.com>
2024-06-27 09:57:58 +0800

    Navbar Updated

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { LuMenuSquare } from "react-icons/lu";
+import { AiFillCloseSquare } from "react-icons/ai";
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
@@ -12,24 +13,27 @@ function Navbar() {
       </button>
       { toggle ? (
         <nav className="h-screen z-10 bg-blue-800 text-white fixed top-0 left-0 md:w-64 w-full flex flex-col justify-start">
+          <div className='flex justify-center items-center'>
+            <button onClick={() => setToggle(false)}><AiFillCloseSquare className='text-5xl mt-3' /></button>
+          </div>
           <div className="flex items-center p-4">
             <h1 className="text-xl font-bold px-2">Tairos</h1>
           </div>
           <ul className="flex flex-col mt-28 px-4">
             <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
-              <a href="/" className="flex items-center">
+              <Link to="/" className="flex items-center">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
-              <a href="/todolists" className="flex items-center">
+              <Link to="/todolists" className="flex items-center">
                 To Do Lists
-              </a>
+              </Link>
             </li>
             <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
-              <a href="/expensetracker" className="flex items-center">
+              <Link to="/expensetracker" className="flex items-center">
                 Expense Tracker
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
