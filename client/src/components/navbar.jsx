3d5a85ec089a66c1bd0dfb11a0205c9a2de8fb44
Lin Khant Htel <linkhanthtel@icloud.com>
2024-07-12 00:26:08 +0800

    UI Updated

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -8,11 +8,11 @@ function Navbar() {
 
   return (
     <div>
-      <button onClick={() => setToggle(!toggle)} className='m-4 text-5xl text-blue-500'>
+      <button onClick={() => setToggle(!toggle)} className='m-4 text-5xl text-blue-900'>
         <LuMenuSquare />
       </button>
       { toggle ? (
-        <nav className="h-screen z-10 bg-blue-800 text-white fixed top-0 left-0 md:w-64 w-full flex flex-col justify-start">
+        <nav className="h-screen z-10 bg-blue-900 text-white fixed top-0 left-0 md:w-64 w-full flex flex-col justify-start">
           <div className='flex justify-center items-center'>
             <button onClick={() => setToggle(!toggle)}><AiFillCloseSquare className='text-5xl mt-3' /></button>
           </div>
@@ -20,17 +20,17 @@ function Navbar() {
             <h1 className="text-xl font-bold px-2">Tairos</h1>
           </div>
           <ul className="flex flex-col mt-28 px-4">
-            <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
+            <li className="hover:bg-blue-800 py-4 px-2 rounded-md">
               <Link to="/" onClick={() => setToggle(false)} className="flex items-center">
                 Home
               </Link>
             </li>
-            <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
+            <li className="hover:bg-blue-800 py-4 px-2 rounded-md">
               <Link to="/todolists" onClick={() => setToggle(false)} className="flex items-center">
                 To Do Lists
               </Link>
             </li>
-            <li className="hover:bg-blue-900 py-4 px-2 rounded-md">
+            <li className="hover:bg-blue-800 py-4 px-2 rounded-md">
               <Link to="/expensetracker" onClick={() => setToggle(false)} className="flex items-center">
                 Expense Tracker
               </Link>
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='bg-white h-screen flex flex-col items-center text-center text-blue-500'>
-      <h1 className='text-3xl my-2'>Home</h1>
-    </div>
+    <motion.div 
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2 }}
+      className='bg-white h-screen flex flex-col items-center text-center text-blue-900'
+    >
+      <h1 className='text-3xl my-2'>Tairos</h1>
+      <p className='w-100 md:w-[800px] text-xl text-center my-4'>Welcome to Tairos Productivity App. In this app, you can make to do lists and expense tracker. To do lists tab will help you list your to do list for your daily life. Expense Tracker tab will help you track your daily expense by managing your income and expense.</p>
+      <div className='flex'>
+        <div className='mx-4 my-5'>
+          <Link className='p-3 border border-blue-900 text-blue-900 rounded-full hover:bg-blue-900 hover:text-white'>Go to To Do Lists Tab</Link>
+        </div>
+        <div className='mx-4 my-5'>
+          <Link className='p-3 border border-blue-900 text-blue-900 rounded-full hover:bg-blue-900 hover:text-white'>Go to Expense Tracker Tab</Link>
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
