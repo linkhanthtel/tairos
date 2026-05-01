2d1bb2efbed5b9cea64d97d2b9eab7f935154c39
Lin Khant Htel <linkhanthtel@icloud.com>
2026-05-02 00:18:14 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,27 @@ import Home from './pages/home'
 import ExpenseTracker from './pages/expensetracker'
 import Calendar from './pages/calendar'
 import Navbar from './components/navbar'
+import Footer from './components/Footer'
 import DemoBanner from './components/DemoBanner'
 import { GlobalProvider } from './context/globalState'
 import { IS_DEMO_BUILD } from './config'
 
 const App = () => {
   return (
-    <div className={`min-h-screen ${IS_DEMO_BUILD ? 'pt-[2.75rem]' : ''}`}>
+    <div className={`flex min-h-screen flex-col ${IS_DEMO_BUILD ? 'pt-[2.75rem]' : ''}`}>
       <GlobalProvider>
         <BrowserRouter>
           {IS_DEMO_BUILD && <DemoBanner />}
           <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/todolists' element={<ToDoLists />} />
-            <Route path='/expensetracker' element={<ExpenseTracker />} />
-            <Route path='/calendar' element={<Calendar />} />
-          </Routes>
+          <main className="flex flex-1 flex-col">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/todolists' element={<ToDoLists />} />
+              <Route path='/expensetracker' element={<ExpenseTracker />} />
+              <Route path='/calendar' element={<Calendar />} />
+            </Routes>
+          </main>
+          <Footer />
         </BrowserRouter>
       </GlobalProvider>
     </div>
